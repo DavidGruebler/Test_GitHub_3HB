d8c4592dd4a46616108ae4b55a6794bbef7ccdc1
DavidGruebler <gruebler@mail.de>
2024-01-16 09:40:12 +0100

    Created getter+setter and methods

--- src/geoMetry/Product.java
+++ src/geoMetry/Product.java
@@ -1,5 +1,29 @@
 package geoMetry;
 
 public abstract class Product {
+	
+	private double y;
+	private double x;
+	
+	public double getY() {
+		return y;
+	}
+	public void setY(double y) {
+		this.y = y;
+	}
+	public double getX() {
+		return x;
+	}
+	public void setX(double x) {
+		this.x = x;
+	}
+	
+	@Override
+	public String toString() {
+		return "Product [y=" + y + ", x=" + x + "]";
+	}
+	
+	public abstract double calculateCircumference();
+	public abstract double calculateArea();
 
 }
