7eb3a76e378df667b7d96a869b3df4ae1c8cefad
DavidGruebler <gruebler@mail.de>
2024-01-21 20:41:57 +0100

    created for class Circle the methods and a constructor

--- src/geoMetry/Circle.java
+++ src/geoMetry/Circle.java
@@ -1,5 +1,27 @@
 package geoMetry;
 
-public class Circle {
+public class Circle extends Product {
+	
+	private double radius;
 
+	public final double pi = 3.14;
+	
+	public Circle(double x, double y, double radius) {
+		this.setX(x);
+		this.setY(y);
+		this.radius = radius;
+	}
+
+	@Override
+	public double calculateCircumference() {
+		return 2 * this.pi * this.radius;
+	}
+ 
+	@Override
+	public double calculateArea() {
+		return this.pi * this.radius * this.radius;
+	}
+	public String toString() {
+		return "Circle:\n\t" + "Radius: " + this.radius;
+	}
 }
