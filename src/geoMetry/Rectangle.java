175d774953b1af55691f9ded0b838b544b6bb649
DavidGruebler <gruebler@mail.de>
2024-01-21 20:41:39 +0100

    created for class Rectangle the methods and a constructor

--- src/geoMetry/Rectangle.java
+++ src/geoMetry/Rectangle.java
@@ -1,5 +1,29 @@
 package geoMetry;
 
-public class Rectangle {
-
+public class Rectangle extends Product {
+	
+	private double length;
+	private double width;
+	
+	public Rectangle(double x, double y, double length, double width) {
+		this.setY(y);
+		this.setX(x);
+		this.length = length;
+		this.width = width;
+	}
+	
+	@Override
+	public double calculateCircumference() {
+		return (2*this.length + (2*this.width));
+	}
+ 
+	@Override
+	public double calculateArea() {
+		return this.length * this.width;
+	}
+ 
+	public String toString() {
+		return "Rectangle:\n\t" + "Length: "  + this.length + "\n\tWidth: " + this.width;
+	}
 }
+
