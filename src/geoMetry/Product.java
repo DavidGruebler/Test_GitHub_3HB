fb60b8c933c6c455731f9578e6047aa75bec9f6c
DavidGruebler <gruebler@mail.de>
2024-01-29 11:28:25 +0100

    Finished the Product class!

--- src/geoMetry/Product.java
+++ src/geoMetry/Product.java
@@ -5,6 +5,13 @@ public abstract class Product {
 	private double y;
 	private double x;
 	
+	public Product(double y, double x) {
+		super();
+		this.y = y;
+		this.x = x;
+	}
+	
+	
 	public double getY() {
 		return y;
 	}
@@ -18,12 +25,26 @@ public abstract class Product {
 		this.x = x;
 	}
 	
+	
+	
 	@Override
 	public String toString() {
-		return "Product [y=" + y + ", x=" + x + "]";
+		return "Calculations [y=" + y + ", x=" + x + "]";
 	}
 	
+	/**
+	 * abstract method for calculations of circumferences
+	 * @return
+	 */
 	public abstract double calculateCircumference();
+	
+	/**
+	 * abstract method for calculations of areas
+	 * @return
+	 */
 	public abstract double calculateArea();
+	
+	
+	
 
 }
