2d69898385f2cfadc285a0c117a01d99fc8a0cbf
DavidGruebler <gruebler@mail.de>
2024-01-29 11:28:36 +0100

    Finished the Rectangle class!

--- src/geoMetry/Rectangle.java
+++ src/geoMetry/Rectangle.java
@@ -1,29 +1,59 @@
 package geoMetry;
 
-public class Rectangle extends Product {
+public class Rectangle extends Product{
 	
 	private double length;
 	private double width;
 	
+	
+	
 	public Rectangle(double x, double y, double length, double width) {
-		this.setY(y);
-		this.setX(x);
+		super(x, y);
 		this.length = length;
 		this.width = width;
 	}
-	
-	@Override
-	public double calculateCircumference() {
-		return (2*this.length + (2*this.width));
+
+
+
+	public double getLength() {
+		return length;
 	}
- 
+
+
+
+	public void setLength(double length) {
+		this.length = length;
+	}
+
+
+
+	public double getWidth() {
+		return width;
+	}
+
+
+
+	public void setWidth(double width) {
+		this.width = width;
+	}
+
+
+
+	@Override
+	/**
+	 * calculates area of Rectangle
+	 */
+	public double calculateCircumference() {
+		return (2*this.length) + (2*this.width);
+	}
+	
+	
+	/**
+	 * calculates area of Rectangle
+	 */
 	@Override
 	public double calculateArea() {
-		return this.length * this.width;
+		return this.length *this.width;
 	}
- 
-	public String toString() {
-		return "Rectangle:\n\t" + "Length: "  + this.length + "\n\tWidth: " + this.width;
-	}
-}
 
+}
