bbd79b02bb134373c5da49f12f04701aa058184e
DavidGruebler <gruebler@mail.de>
2024-01-30 09:37:54 +0100

    Created the class Comments

---
+++ src/theSocialNetwork/Comments.java
@@ -0,0 +1,33 @@
+package theSocialNetwork;
+
+public class Comments {
+	
+	private String userName;
+	private String message;
+
+	public Comments(String userName, String message) {
+		super();
+		this.userName = userName;
+		this.message = message;
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "Comments [userName=" + userName + ", message=" + message + "]";
+	}
+	
+
+}
