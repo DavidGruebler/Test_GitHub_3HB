dfb0e46198beb9103a27e8d8b439ecb2981e1034
DavidGruebler <gruebler@mail.de>
2024-03-12 09:45:33 +0100

    ArrayQueue

--- src/linkedList/CheeseInserter.java
+++ src/linkedList/CheeseInserter.java
@@ -1,13 +1,19 @@
-package linkedList;
+	package linkedList;
 
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.regex.Pattern;
 
 public class CheeseInserter {
 	
+	
+	//private - Sichtbarkeitsbereich auf Klassen beschränkt
+	//static - von der Klasse abhängig und nicht vum Objekt
+	//final - Variable nach Initialisierung unveränderbar
 	private static final Pattern vegetables = Pattern.compile(
-			"Zucchini|Paprikas?|Zwiebeln?1Tomaten?"
+			"Zucchini|Paprikas?|Zwiebeln?|Tomaten?|Melanzani"
 			);
 	
 	public static void insertCheeseAroundVegetable(List<String> ingredients) {
@@ -15,13 +21,23 @@ public class CheeseInserter {
 		//Abbruchbedingung hasNext
 		for(ListIterator<String> iterator = ingredients.listIterator();iterator.hasNext();) {
 			String ingedient = iterator.next();
+			//macher - wird verwendet, um ingredient in unserem Muster zu finden
+			//machers() - für den direckten Vergleich 
+			if(vegetables.matcher(ingedient).matches()) {
+				iterator.add("Käse");
+				
+			}
 			
 		}
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		List<String> ingredients = new LinkedList<String>();
+		Collections.addAll(ingredients, "Gnocchi","Paprika","Tomate","Pfeffer","Melanzani");
+		insertCheeseAroundVegetable(ingredients);
+		System.out.println(ingredients);
+  
 	}
 
 }
---
+++ src/queue/ArrayQueue.java
@@ -0,0 +1,36 @@
+package queue;
+
+public class ArrayQueue {
+	
+	private static int front;
+	private static int rear;
+	private static int capacity;
+	private static int[] queue;
+
+	public ArrayQueue(int size) {
+		front = 0;
+		rear = 0;
+		capacity = size;
+		queue = new int[size];
+	}
+	
+	
+	public static void queueEnqueu(int item) {
+		if(rear == capacity) {
+			System.out.println("Queue ist voll.");
+		} else {
+			queue[rear]=item;
+			rear++;
+		}	
+	}
+	
+	public static int queueDequeue() {
+		if(rear == front) {
+			System.out.println("Queue ist leer");
+		} else {
+			int fistElemtent = queue[front];
+			
+		}
+	}
+
+}
