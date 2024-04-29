f97070d0d4da834d14f12241b48718670cedf587
DavidGruebler <gruebler@mail.de>
2024-04-29 10:40:23 +0200

    started task 4

---
+++ src/stringBuilder/CamelCase.java
@@ -0,0 +1,21 @@
+package stringBuilder;
+
+public class CamelCase {
+	
+	
+	public static String camelCaseSplitter(String input) {
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(char c : input.toCharArray()) {
+			if()
+		}
+			
+	}
+
+	public static void main(String[] args) {
+		
+	
+	}
+
+}
